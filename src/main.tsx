651a80e31726c76f5e87b5d1a75ba80189811794
algarcia-vector <algarcia@vector.com.mx>
2025-11-25 15:17:56 -0600

    Configurar proyecto para GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
+const basePath = import.meta.env.PROD ? "/portfolio" : "";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basePath}>
       <App />
     </BrowserRouter>
   </StrictMode>
