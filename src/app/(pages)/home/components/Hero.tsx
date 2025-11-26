3cf8cf2ef6acbea450fdac04729598b23153dcf3
algarcia-vector <algarcia@vector.com.mx>
2025-11-25 18:40:04 -0600

    prueba original

--- src/app/(pages)/home/components/Hero.tsx
+++ src/app/(pages)/home/components/Hero.tsx
@@ -25,14 +25,15 @@ const Hero = () => {
             className="text-center px-0 px-xl-4 mt-5 mt-lg-0 pt-5 pt-lg-0"
           >
             <h5 className="d-inline-block py-1 px-3 rounded text-muted font-secondary">
-              Hi, I'm Alberto García
+              Hi, I'm David Williamson
             </h5>
             <h1 className="hero-title mb-4 font-secondary fo">
-              I'm a Senior{" "}
+              I'm a freelance{" "}
               <mark>
+                {" "}
                 <Typist
                   className="fw-medium typewrite"
-                  sentences={["Mobile", "Web"]}
+                  sentences={["React", "Flutter", "Python"]}
                   typingSpeed={1500}
                   deletingSpeed={700}
                   showCursor={false}
@@ -40,8 +41,8 @@ const Hero = () => {
                   cursorSmooth
                   pauseTime={2500}
                 />
-              </mark>
-              Engineer
+              </mark>{" "}
+              Developer
             </h1>
             <span className="wrap" />
             <div className="mb-5 mb-lg-0">
